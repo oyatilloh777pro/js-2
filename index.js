aae55b9870439c97f1faf91d5cc8b1c46385f598
Oyatilloh <oyatillohqobiljonov81@gmail.com>
2024-04-12 12:02:28 +0500

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+let matn = String(prompt("String turdagi ma'lumot kiriting: "));
+let son = Number(prompt("Number turdagi ma'lumot kiriting: "));
+let rost = Boolean(prompt("Boolean turdagi ma'lumot kiriting: "));
+
+confirm("Hamma ma'lumotlar to'g'rimi?")
+
+alert("Sizning kiritgan " + matn + " so'zi " + typeof matn + " typga tegishli!");
+alert("Sizning kiritgan " + son + " soni " + typeof son + " typga tegishli!");
+alert("Sizning kiritgan " + rost + " rost " + typeof rost + " typga tegishli!");
